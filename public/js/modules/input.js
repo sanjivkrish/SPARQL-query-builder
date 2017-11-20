f5ab366c2eea19f4a6a5aea97c6f10da6f352623
Marcel Reppenhagen <marcel.reppenhagen@campus.tu-berlin.de>
2017-11-20 16:55:49 +0100

    added throttle to input

--- public/js/modules/input.js
+++ public/js/modules/input.js
@@ -17,6 +17,18 @@ PREFIX owl: <http://www.w3.org/2002/07/owl#>
 SELECT DISTINCT ?prop WHERE { { ?prop a rdf:Property } UNION { ?prop a owl:ObjectProperty } UNION { ?prop a owl:DatatypeProperty } FILTER ( REGEX(str(?prop), "${string}", 'i') )FILTER ( !( REGEX(str(?prop), "^(http://www.w3.org/2002/07/owl#|http://www.openlinksw.com/|nodeID://)", 'i') ) ) } LIMIT 200`
 }
 
+function throttle(callback, delay) {
+  let previousCall = new Date().getTime();
+  return function() {
+    let currentCall = new Date().getTime();
+
+    if ((currentCall - previousCall) >= delay) {
+      previousCall = currentCall;
+      callback.apply(null, arguments);
+    }
+  };
+}
+
 function executeQuery(endpoint, query) {
   return axios({
     url: endpoint,
@@ -28,41 +40,28 @@ function executeQuery(endpoint, query) {
   })
 }
 
-const input = $('#bla')
-input.addEventListener('input', function(e) {
-  if (this.value == '') {
+function onInput(e) {
+  if (e.target.value == '') {
     return
   }
-  const classQuery = constructClassQuery(this.value)
-  const propertyQuery = constructPropertyQuery(this.value)
-  // axios({
-  //   url: endpoint,
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded'
-  //   },
-  //   params: { query } // using params instead of data because of urlencoded data
-  // })
-  //   .then(response => {
-  //     const resultDiv = $('.result')
-  //     resultDiv.innerHTML = '' // clear previous response
-  //     response.data.results.bindings.forEach( result => {
-  //       const div = document.createElement('div')
-  //       div.textContent = result.class.value
-  //       resultDiv.appendChild(div)
-  //     })
-  //   })
+  const classQuery = constructClassQuery(e.target.value)
+  const propertyQuery = constructPropertyQuery(e.target.value)
+
+  // make SPARQL endpoint codes
   const classPromise = executeQuery(endpoint, classQuery)
   const propertyPromise = executeQuery(endpoint, propertyQuery)
 
   const resultDiv = $('.result')
   resultDiv.innerHTML = '' // clear previous response
+
+  // wait for all responses to come back
   Promise.all([classPromise, propertyPromise])
     .then( response => {
       const [classResults, propertyResults] = response
       const classDiv = document.createElement('div')
       const propertyDiv = document.createElement('div')
 
+      // loop through results and add new div for each result
       classResults.data.results.bindings.forEach( result => {
         const div = document.createElement('div')
         div.textContent = result.class.value
@@ -75,7 +74,12 @@ input.addEventListener('input', function(e) {
         propertyDiv.appendChild(div)
       })
 
+      // add results to the page
       resultDiv.appendChild(classDiv)
       resultDiv.appendChild(propertyDiv)
     })
-})
+    .catch( err => console.error(err))
+}
+
+const input = $('#topicInput')
+input.addEventListener('input', throttle(onInput.bind(this), 500))
